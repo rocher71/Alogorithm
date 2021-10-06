faacac380ff5d6fb1a490b2ec5e16ffa1c28b984
rocher71 <qja086351@naver.com>
2021-10-06 18:25:10 +0900

    20211006 BOJ solved (silver)

--- Baekjoon Online Judge/5635_생일.cpp
+++ Baekjoon Online Judge/5635_생일.cpp
@@ -11,11 +11,12 @@
 using namespace std;
 
 int n, year, month, date;
-string name;
+string name, youngest, oldest;
 vector<string> nameVec;
 vector<int> dateVec, monthVec;
 vector<pair<int, int>> yearVec; //year, idx
-vector<pair<int, int>> youngVec, oldVec;
+vector<pair<int, int>> youngVec, oldVec, oldSameMonth;
+
 
 int main()
 {
@@ -34,42 +35,118 @@ int main()
 
 	sort(yearVec.begin(), yearVec.end());
 
-	int young = 9999, old = 0;
+	int young = 0, old = 9999;
 	for (int i = 0; i < n; i++)
 	{
+		int idx = yearVec[i].second;
 		if (yearVec[i].first == young)
 		{
-			youngVec.push_back({ yearVec[i].first, i });
+			youngVec.push_back({ yearVec[i].first, idx });
 		}
-		else if(yearVec[i].first < young)
+		else if(yearVec[i].first > young)
 		{
 			young = yearVec[i].first;
 			youngVec.clear();
-			youngVec.push_back({ yearVec[i].first, i });
+			youngVec.push_back({ yearVec[i].first, idx });
 		}
 
 		if (yearVec[i].first == old)
 		{
-			oldVec.push_back({ yearVec[i].first, i });
+			oldVec.push_back({ yearVec[i].first, idx });
 		}
-		else if (yearVec[i].first > old)
+		else if (yearVec[i].first < old)
 		{
-			young = yearVec[i].first;
+			old = yearVec[i].first;
 			oldVec.clear();
-			oldVec.push_back({ yearVec[i].first, i });
+			oldVec.push_back({ yearVec[i].first, idx });
 		}
 	}
+	
+	if (youngVec.size() == 1)
+	{
+		youngest = nameVec[youngVec[0].second];
+	}
+	if (oldVec.size() == 1)
+	{
+		oldest = nameVec[oldVec[0].second];
+	}
 
-	int leastMonth = 13, mostMonth = 0;
+	if (youngVec.size() == 1 && oldVec.size() == 1)
+	{
+		cout << youngest << "\n" << oldest << "\n";
+		return 0;
+	}
+
+	yearVec.clear();
+	int youngMonth = 0, oldMonth = 13;
+	//연도 같은 사람들의 달 비교
 	for (int i = 0; i < youngVec.size(); i++)
 	{
-		yearVec.clear();
-		if (monthVec[youngVec[i].second] < leastMonth)
+		int idx = youngVec[i].second;
+		if (youngVec.size() == 1) //젤 어린 연도 한명일 때
 		{
+			yearVec.push_back({ monthVec[idx], idx });
+			break;
+		}
 
+		if (monthVec[idx] > youngMonth) //젤 어린 애 달이 한명이면 변수 벡터 초기화
+		{
+			youngMonth = monthVec[idx];
+			yearVec.clear();
+			yearVec.push_back({ monthVec[idx], idx });
+		}
+		else if (monthVec[idx] == youngMonth)
+		{
+			yearVec.push_back({ monthVec[idx], idx });
 		}
 	}
 
+	for (int i = 0; i < oldVec.size(); i++)
+	{
+		int idx = oldVec[i].second;
 
+		if (oldVec.size() == 1) //젤 많은 연도 한명일 때
+		{
+			oldSameMonth.push_back({ monthVec[idx], idx });
+			break;
+		}
+
+		if (monthVec[idx] < oldMonth) //젤 많은 달이 한명이면 변수 벡터 초기화
+		{
+			oldMonth = monthVec[idx];
+			oldSameMonth.clear();
+			oldSameMonth.push_back({ monthVec[idx], idx });
+		}
+		else if (monthVec[idx] == oldMonth)
+		{
+			oldSameMonth.push_back({ monthVec[idx], idx });
+		}
+	}
+
+	if (yearVec.size() == 1)
+	{
+		youngest = nameVec[yearVec[0].second];
+	}
+	if (oldSameMonth.size() == 1)
+	{
+		oldest = nameVec[oldSameMonth[0].second];
+	}
+
+	if (yearVec.size() == 1 && oldSameMonth.size() == 1)
+	{
+		cout << youngest << "\n" << oldest << "\n";
+		return 0;
+	}
+
+
+	sort(yearVec.begin(), yearVec.end());
+	sort(oldSameMonth.begin(), oldSameMonth.end());
+
+	youngest = nameVec[yearVec[yearVec.size()-1].second];
+	oldest = nameVec[oldSameMonth[0].second];
+
+	cout << youngest << "\n" << oldest << "\n";
+
+	return 0;
 
 }

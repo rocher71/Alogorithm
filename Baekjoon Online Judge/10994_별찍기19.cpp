50ec7d3f488048195e7bbb01a6720971e42e207c
rocher71 <qja086351@naver.com>
2021-10-21 18:13:20 +0900

    ***********gold 별찍기 푸는중***********

---
+++ Baekjoon Online Judge/10994_별찍기19.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<vector>
+#include<map>
+#include<stack>
+#include<string>
+#include<cstring>
+#include<sstream>
+#include<deque>
+using namespace std;
+
+int n, l;
+vector<int> lengths;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+	
+	int len = 1 + 4 * (n - 1);
+	for (int i = n; i >=1 ; i--)
+	{
+		l = 1 + 4 * (i - 1);
+		lengths.push_back(l);
+	}
+
+	
+	
+	
+	/*for (int i = 0; i < length; i++)
+	{
+		cout << "*";
+		if (i == 0 || i == length - 1)
+		{
+			for (int j = 0; j < length - 2; j++)
+				cout << "*";
+		}
+		else if ( i == 1 || i == length -2)
+		{
+			for (int j = 0; j < length - 2; j++)
+				cout << " ";
+		}
+		else if (i == length / 2)
+		{
+			for (int j = 0; j < length - 2; j++)
+			{
+				if (j % 2 == 0)
+					cout << " ";
+				else
+					cout << "*";
+			}
+		}
+		else
+		{
+			cout << " *";
+			for (int j = 0; j < length - 4; j++)
+			{
+				if (j % 2 == 0)
+				{
+					cout << "*";
+				}
+				else
+				{
+
+				}
+			}
+			cout << "* " ;
+		}
+		cout << "*\n";
+	}*/
+
+
+
+
+
+}

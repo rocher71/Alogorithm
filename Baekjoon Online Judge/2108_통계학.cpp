f9128da41fe4994b0d33f6f427e4187436fcc46f
rocher71 <qja086351@naver.com>
2021-11-17 16:55:55 +0900

    *********** silver 통계학푸는중 ***************

--- Baekjoon Online Judge/2108_통계학.cpp
+++ Baekjoon Online Judge/2108_통계학.cpp
@@ -36,39 +36,65 @@ int freq()
 {
 	if (v.size() == 1) return v[0];
 
-	vector<pair<int,int>> freV; //빈도, 값
+	//vector<pair<int,int>> frev; //빈도, 값
 
-	int prevNum = v[0], num = 0;
-	for (int i = 1; i < n; i++)
+	//int prevnum = v[0], num = 0;
+	//for (int i = 1; i < n; i++)
+	//{
+	//	//중복x
+	//	if (v[i] != v[i - 1] && num == 0) continue;
+
+	//	//중복된 애들의 연속 끝
+	//	if (v[i] != v[i - 1]) 
+	//	{
+	//		frev.push_back({ num, v[i - 1] });
+	//		num = 0;	
+	//		continue;
+	//	}
+
+	//	if (i == n-1 && v[i] == v[i - 1])
+	//	{
+	//		num++;
+	//		frev.push_back({ num, v[i - 1] });
+	//		continue;
+	//	}
+
+	//	//중복 ing
+	//	num++;
+	//	
+	//}
+
+	//sort(frev.begin(), frev.end());
+	//if (frev.size() == 0) return v[1];
+	//if (frev.size() == 1) return frev[0].second;
+	//
+	//return frev[1].second;
+
+	map<int, int> cntM; //{숫자, 개수}
+	for (int i = 0; i < v.size(); i++)
 	{
-		//중복x
-		if (v[i] != v[i - 1] && num == 0) continue;
-
-		//중복된 애들의 연속 끝
-		if (v[i] != v[i - 1]) 
+		if (cntM.find(v[i]) != cntM.end()) //한번 들어온 수면
 		{
-			freV.push_back({ num, v[i - 1] });
-			num = 0;	
+			cntM.find(v[i])->second++;
 			continue;
 		}
-
-		if (i == n-1 && v[i] == v[i - 1])
-		{
-			num++;
-			freV.push_back({ num, v[i - 1] });
-			continue;
-		}
-
-		//중복 ing
-		num++;
-		
+		//처음 들어온 수면
+		cntM.insert({ v[i], 1 });
 	}
 
-	sort(freV.begin(), freV.end());
-	if (freV.size() == 0) return v[1];
-	if (freV.size() == 1) return freV[0].second;
-	
-	return freV[1].second;
+	vector<pair<int, int>> cntNums; //{개수, 숫자}
+	for (auto iter : cntM)
+	{
+		cntNums.push_back({ iter.second, iter.first });
+	}
+
+	sort(cntNums.begin(), cntNums.end());
+	if (cntNums.size() <= 1)	return cntNums[0].second;
+
+	//두개 이상, 같은 개수 여러개인 경우
+	if (cntNums[0].second != cntNums[1].second)	return cntNums[1].second;
+	return cntNums[0].second;
+
 }
 
 int range()
